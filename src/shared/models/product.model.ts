0fd30411651244bffeee93f92bec3fa39764ccf2
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-08 06:18:22 -0700

    feat(server/shared): add new properties in product collection

--- src/shared/models/product.model.ts
+++ src/shared/models/product.model.ts
@@ -4,5 +4,11 @@ export interface Product {
     name: string,
     price: number,
     quantity: number,
-    imgUrl?: string
+    imgUrl?: string,
+    category: string,
+    author?: string,// book
+    colors?: [string], // clothes etc
+    sizes?: [string], // clothes(S,XS,M,L,Xl etc.)
+    artists?: [string], // music
+    subCategory?: string, // (men,women,boy,girl etc)
 }
