feacaaa2e13bfac67d6f214c052ceeabe566ae4e
Aman-codde <78620905+Aman-codde@users.noreply.github.com>
2021-10-26 06:42:42 -0500

    fix(client): remove selector from component

--- src/client/src/app/components/product-details/product-details.component.ts
+++ src/client/src/app/components/product-details/product-details.component.ts
@@ -14,24 +14,24 @@ import { Product } from '../../../../../shared/models/product.model.js';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  product$: Observable<Product | null>;
-  selectedProduct$: Observable<Product | null>
-  selectedProduct : Product| null = null; // new
+  //product$: Observable<Product | null>;
+  //selectedProduct$: Observable<Product | null>
+  //selectedProduct : Product| null = null; // new
 
   constructor(
     private store: Store<AppState>,
     private route: ActivatedRoute,
   ) 
   { 
-    this.product$ = this.store.select(productSelector);
-    this.selectedProduct$ = this.store.select(selectedProductSelector);
+    //this.product$ = this.store.select(productSelector);
+    //this.selectedProduct$ = this.store.select(selectedProductSelector);
    
   }
 
   ngOnInit(): void {
-    this.selectedProduct$.subscribe(data => this.selectedProduct = data);
-    const id = ""+this.selectedProduct?._id
-    this.store.dispatch(loadProduct({data: id}))
+    //this.selectedProduct$.subscribe(data => this.selectedProduct = data);
+    //const id = ""+this.selectedProduct?._id
+    //this.store.dispatch(loadProduct({data: id}))
 
     
   }
